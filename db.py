919cdc303e991aa8876a2aa11d3dafd653dd7862
Stanislav Shulha <stshlaptop@gmail.com>
2021-10-21 22:35:00 +0300

    ~ Minor code style changes: removed explicit creation of cursor objects

--- db.py
+++ db.py
@@ -18,57 +18,49 @@ def connect(database: str) -> sqlite3.Connection:
     return sqlite3.connect(database)
 
 
-def ingest_teams(teams_data: Dict[str, list], connection: sqlite3.Connection) -> None:
+def ingest_teams(teams_data: Dict[str, list], conn: sqlite3.Connection) -> None:
     """Inserts parsed data for teams into a database."""
-    cursor = connection.cursor()
     for noc_name, values in teams_data.items():
         name, team_id = values  # Unpacking name and id
-        cursor.execute(
-            f"insert into {TEAMS} values (?, ?, ?)", (team_id, name, noc_name)
-        )
+        conn.execute(f"insert into {TEAMS} values (?, ?, ?)", (team_id, name, noc_name))
 
 
-def ingest_games(games_data: Dict[str, list], connection: sqlite3.Connection) -> None:
+def ingest_games(games_data: Dict[str, list], conn: sqlite3.Connection) -> None:
     """Ingests parsed data for games into a database."""
-    cursor = connection.cursor()
     for game_info in games_data.values():
         city, season, year, game_id = game_info
-        cursor.execute(
+        conn.execute(
             f"insert into {GAMES} values (?, ?, ?, ?)",
             (game_id, year, season, city),
         )
 
 
-def ingest_athletes(athletes_data: Dict[str, dict], connection: sqlite3.Connection) -> None:
+def ingest_athletes(athletes_data: Dict[str, dict], conn: sqlite3.Connection) -> None:
     """Ingests parsed data for athletes into a database."""
-    cursor = connection.cursor()
     for athlete in athletes_data.values():
         name, sex, year_of_birth, parameters, athlete_id, team_id = athlete.values()
-        cursor.execute(
+        conn.execute(
             f"insert into {ATHLETES} values (?, ?, ?, ?, ?, ?)",
             (athlete_id, name, year_of_birth, sex, str(parameters), team_id),
         )
 
 
-def ingest_sports(sports_data: Dict[str, int], connection: sqlite3.Connection) -> None:
+def ingest_sports(sports_data: Dict[str, int], conn: sqlite3.Connection) -> None:
     """Ingests parsed data for sports into a database."""
-    cursor = connection.cursor()
     for sport, sport_id in sports_data.items():
-        cursor.execute(f"insert into {SPORTS} values (?, ?)", (sport_id, sport))
+        conn.execute(f"insert into {SPORTS} values (?, ?)", (sport_id, sport))
 
 
-def ingest_events(events_data: Dict[str, int], connection: sqlite3.Connection) -> None:
+def ingest_events(events_data: Dict[str, int], conn: sqlite3.Connection) -> None:
     """Ingests parsed data for events into a database."""
-    cursor = connection.cursor()
     for event, event_id in events_data.items():
-        cursor.execute(f"insert into {EVENTS} values (?, ?)", (event_id, event))
+        conn.execute(f"insert into {EVENTS} values (?, ?)", (event_id, event))
 
 
-def ingest_results(results_data: List[tuple], connection: sqlite3.Connection) -> None:
+def ingest_results(results_data: List[tuple], conn: sqlite3.Connection) -> None:
     """Ingests parsed data for results into a database."""
-    cursor = connection.cursor()
     for result in results_data:
-        cursor.execute(
+        conn.execute(
             f"insert into {RESULTS}(athlete_id, game_id, sport_id, event_id, medal) "
             f"values (?, ?, ?, ?, ?)",
             result,
