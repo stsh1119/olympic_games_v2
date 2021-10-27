0c0e2cda4f0aa6c49d4acce70ecf833f74a87d5a
Stanislav Shulha <stshlaptop@gmail.com>
2021-10-28 02:19:54 +0300

    + Added top-teams chart, code style and naming changes

--- stats.py
+++ stats.py
@@ -33,49 +33,27 @@ def _get_all_teams() -> List[str]:
         return [team[0] for team in conn.execute("select noc_name from teams").fetchall()]
 
 
-def _get_game_years() -> List[int]:
-    """Gets all years, when olympic games were conducted.
-
-    Returns:
-        A list of game years, for example [1992, 2012, 1920, 1900...].
-    """
-    with sqlite3.connect(DATABASE) as conn:
-        return [year[0] for year in conn.execute("select year from games").fetchall()]
-
-
-def sanitize_input() -> tuple:
-    """Parses & validates user input to make sure chart type is correct and needed params are present."""
-    user_input = sys.argv[1:]
-    if len(user_input) == 0:
-        raise BadInput('No arguments given, try giving some, available charts: medals/top-teams')
-    chart_type, *params = user_input
-    if chart_type == 'medals':
-        try:  # Checking whether given mandatory parameters are valid
-            season: int = [SEASONS.get(value.lower()) for value in params if value.lower() in SEASONS][0]
-            noc_name: str = [value.upper() for value in params if value.upper() in _get_all_teams()][0]
-            # Validating optional parameters
-            medal = [MEDALS.get(value) for value in params if value in MEDALS]
-            if medal:
-                return season, noc_name, medal[0]
-            return season, noc_name
-        except IndexError as index_missing:
-            raise BadInput('Both NOC name and season parameters are required, medal is optional.') from index_missing
-
-    elif chart_type == 'top-teams':
-        pass
-    else:
-        raise BadInput('Chart unavailable: try medals/top-teams.')
-
-
-def get_medals_stats(valid_input: tuple) -> List[tuple]:
+def get_medals_stats() -> List[tuple]:
     """Given valid parameters, queries for data that will be used to build a chart.
 
     Returns:
         List of tuples, each containing year, amount of medals for that year, example [(1988, 1), (1992, 5),...]
     """
+    user_query_params = sys.argv[2:]  # Provided by user
+    try:  # Checking whether given mandatory parameters are valid
+        season: int = [SEASONS.get(value.lower()) for value in user_query_params if value.lower() in SEASONS][0]
+        noc_name: str = [value.upper() for value in user_query_params if value.upper() in _get_all_teams()][0]
+        # Validating optional parameters
+        medal = [MEDALS.get(value) for value in user_query_params if value in MEDALS]
+        medal = medal[0] if medal else None
+    except IndexError as index_missing:
+        raise BadInput('Both NOC name and season parameters are required, medal is optional.') from index_missing
+
+    valid_params = tuple(filter(lambda x: x if x else False, (season, noc_name, medal)))
+
     with sqlite3.connect(DATABASE) as conn:
         cursor = conn.cursor()
-        db_results = cursor.execute(
+        query_results = cursor.execute(
             "select g.year, count(r.medal) as amount_of_medals "
             "from results r, athletes a, teams t, games g "
             "where r.athlete_id = a.id "
@@ -83,56 +61,83 @@ def get_medals_stats(valid_input: tuple) -> List[tuple]:
             "and g.id = r.game_id "
             "and g.season = ? "  # required
             "and t.noc_name = ? "  # required
-            f"{'and medal = ? ' if len(valid_input) == 3 else ''}"  # checking if medal is specified
+            f"{'and medal = ? ' if medal else ''}"  # checking if medal is specified
             "group by g.year "
             "order by g.year",
-            valid_input,
+            valid_params,
         ).fetchall()
 
-        return db_results
+        return query_results
 
 
-def get_top_teams_stats(valid_input: tuple) -> List[tuple]:
+def get_top_teams_stats() -> List[tuple]:
     """Given valid parameters, queries for data that will be used to build top-teams chart.
 
-        Returns:
-            List of tuples, each containing team, amount of medals for it, example [('AUS', 77), ('USA, 76),...]
+    Returns:
+        List of tuples, each containing team, amount of medals for it, example [('AUS', 77), ('USA, 76),...]
     """
+    user_query_params = sys.argv[2:]  # Provided by user
+    if len(user_query_params) < 1:
+        raise BadInput('Missing mandatory parameter: season')
+    try:  # Validating mandatory parameter
+        season: int = [SEASONS.get(value.lower()) for value in user_query_params if value.lower() in SEASONS][0]
+    except IndexError as missing_index:
+        raise BadInput('Incorrect mandatory parameter: season') from missing_index
+    # Checking for optional parameters
+    medal = [MEDALS.get(value) for value in user_query_params if value in MEDALS]
+    medal = medal[0] if medal else None
+    year = [int(value) for value in user_query_params if value.isdigit()]
+    year = year[0] if year else None
+
+    valid_params = tuple(filter(lambda x: x if x else False, (season, medal, year)))
+
     with sqlite3.connect(DATABASE) as conn:
-        medal, season, year = valid_input
-        db_results = conn.execute("with amount_per_team as (select t.noc_name, count(medal) medals_amount "
-                                  "from teams t, results r, athletes a, games g "
-                                  "where t.id = a.team_id "
-                                  "and a.id = r.athlete_id "
-                                  "and g.id = r.game_id "
-                                  "and r.medal = ? "
-                                  "and g.season = ? "  # required
-                                  "and g.year = ? "
-                                  "group by t.noc_name "
-                                  "order by medals_amount desc) "
-                                  "select * "
-                                  "from amount_per_team "
-                                  "where medals_amount > (select avg(medals_amount) from amount_per_team)",
-                                  (medal, season, year)
-                                  ).fetchall()
-        return db_results
+        query_results = conn.execute(
+            "with amount_per_team as (select t.noc_name, count(medal) medals_amount "
+            "from teams t, results r, athletes a, games g "
+            "where t.id = a.team_id "
+            "and a.id = r.athlete_id "
+            "and g.id = r.game_id "
+            "and g.season = ? "  # required
+            f"{'and r.medal = ? ' if medal else ''}"
+            f"{'and g.year = ? ' if year else ''}"
+            "group by t.noc_name "
+            "order by medals_amount desc) "
+            "select * "
+            "from amount_per_team "
+            "where medals_amount > (select avg(medals_amount) from amount_per_team)",
+            valid_params
+        ).fetchall()
+        return query_results
 
 
-def build_medals_chart(db_medal_stats: List[tuple]) -> None:
+def build_chart(query_result: List[tuple]) -> None:
     """Determines a coefficient(k), so that chart is no longer than 200 blocks and
-    builds a bar chart for a given data."""
-    max_amount_of_medals = max(db_medal_stats, key=lambda x: x[1])[1]
-    k = MAX_BAR_LENGTH / max_amount_of_medals
+    builds a bar chart for a given data.
 
-    for year_data in db_medal_stats:
-        year, amount_of_medals = year_data[0], int(year_data[1])
-        print(year, int(round(amount_of_medals * k)) * BLOCK)
+    Args:
+        query_result: List of tuples returned from the DB after query execution.
+
+        """
+    max_numeric_value = max(query_result, key=lambda x: x[1])[1]
+    k = MAX_BAR_LENGTH / max_numeric_value
+
+    for row in query_result:
+        dimension, metric = row[0], int(row[1])
+        print(dimension, int(round(metric * k)) * BLOCK)
 
 
 if __name__ == '__main__':
     try:
-        parameters = sanitize_input()
-        medal_stats = get_medals_stats(parameters)
-        build_medals_chart(medal_stats)
+        if len(sys.argv[1:]) == 0:
+            raise BadInput('No arguments given, try giving some, available charts: medals/top-teams')
+        if sys.argv[1] == 'medals':
+            data_from_db = get_medals_stats()
+        elif sys.argv[1] == 'top-teams':
+            data_from_db = get_top_teams_stats()
+        else:
+            raise BadInput('Chart unavailable: try medals/top-teams.')
+        build_chart(data_from_db)
+
     except BadInput as err:
         print(str(err))
